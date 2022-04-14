44cda99bfdd9831d791f1cd7970b79ccbda490a5
CipiVlad <supercoder.cipi@gmail.com>
2022-04-14 14:28:06 +0200

    params

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+
+
+function intro3(name, stadt, alter) {
+
+    let bioPic = "Hi, mein Name ist " + name + ". " + "Ich bin " + alter +
+        " Jahre alt. Ich komme aus " + stadt + ".";
+    console.log(bioPic);
+}
+
+intro3("Cipi", "Tübingen", "37");
